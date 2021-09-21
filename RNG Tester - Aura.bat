4c53afa23affbd15b20a46837e781972fc79d28d
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-09-21 10:41:17 -0400

    Update RNG Tester - Aura.bat

--- RNG Tester - Aura.bat
+++ RNG Tester - Aura.bat
@@ -1,12 +1,18 @@
 @echo off
 title RNG Tester
 set /a tries=0
+set /a moneygain=0
 MODE con: lines=3
 
 :askchance
 	cls
 	set /p chance=Chances of getting number: 1/
 	set /a chance="%chance%"+1
+	goto askmoney
+	
+:askmoney
+	cls
+	set /p money=How much money will you get each drop?
 	goto asklog
 	
 :asklog
@@ -25,8 +31,8 @@ MODE con: lines=3
 	set /a num=%random% %%%chance%
 	set /a tries=%tries%+1
 	set /a timespent=%tries%*%time%
-	echo Number Rolled: %num% Try #%tries% Time Spent: %timespent%
-	if "%num%"=="1" goto stop
+	echo Number Rolled: %num% Try #%tries% Time Spent: %timespent% Money Gained: %moneygain%
+	if "%num%"=="1" goto claim
 	timeout /t %time% >nul
 	goto autolog
 	
@@ -38,11 +44,11 @@ MODE con: lines=3
 :auto
 	cls
 	set /a num=%random% %%%chance%
-	echo Number Rolled: %num%
-	if "%num%"=="1" goto stop
+	echo Number Rolled: %num% Money Gained: %moneygain%
+	if "%num%"=="1" goto claim
 	timeout /t %time% >nul
 	goto auto
 	
-:stop
-	timeout /t 60
+:claim
+	set /a moneygain=%moneygain%+%money%
 	exit
