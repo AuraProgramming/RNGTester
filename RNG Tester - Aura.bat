1a3867fff0da80e36a3734b5c6bdda7f2e66f6fc
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-09-22 10:39:10 -0400

    Update RNG Tester - Aura.bat

--- RNG Tester - Aura.bat
+++ RNG Tester - Aura.bat
@@ -15,20 +15,31 @@ MODE con: lines=3
 :askmoney
 	cls
 	set /p money=How much money will you get each drop? Enter Number: 
-	goto asklog
+	goto askname
 	
-:asklog
+:askname
+	cls
+	set /p name=What is the  name of the rare drop (No spaces): 
+	goto askshow
+	
+:askshow
 	cls
 	set /p trytf=Would you like to show tries and time? Y/N: 
-	if "%trytf%"=="Y" goto autologsetup
+	if "%trytf%"=="Y" goto autotrysetup
 	if "%trytf%"=="N" goto autosetup
 
-:autologsetup
+:asklog
+	cls
+	set /p data=Would you like your drop data to be logged? Y/N: 
+	if "%datatf%"=="Y" goto checktrylog
+	if "%datatf%"=="N" goto checktry
+
+:autotrysetup
 	cls
 	set /p time=Time in between (Min.1): 
-	goto autolog
+	goto autotry
 	
-:autolog
+:autotry
 	cls
 	set /a num=%random% %%%chance%
 	set /a tries=%tries% + 1
@@ -38,7 +49,7 @@ MODE con: lines=3
 	echo Number Rolled: %num% / Try #%tries% / Time Spent: %timespent% seconds / Rare Drops: %raredrops% / Money Gained: %moneygain% / Estimated Money per Minute: %moneyminute%
 	if "%num%"=="1" goto claim
 	timeout /t %time% >nul
-	goto autolog
+	goto autotry
 	
 :autosetup
 	cls
@@ -60,9 +71,11 @@ MODE con: lines=3
 :claim
 	set /a raredrops=%raredrops% + 1
 	set /a moneygain=%moneygain%+%money%
-	goto autolog
+	if "%logtf%"=="Y" echo Timespent: %timespent% seconds Tries: %tries% Estimated Money per Minute: %moneyminute%>>%name%.txt
+	goto autotry
 	
 :claim2
 	set /a raredrops=%raredrops% + 1
 	set /a moneygain=%moneygain%+%money%
+	if "%logtf%"=="Y" echo Timespent: %timespent% seconds Tries: %tries% Estimated Money per Minute: %moneyminute%>>%name%.txt
 	goto auto
